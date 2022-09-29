ae47704e188036b56f31439a37b101f76dbdf7c4
pi-rate14 <apoorvasrivastava.14@gmail.com>
2022-09-29 20:42:30 +0530

    Transfer API and currency validator

--- util/random.go
+++ util/random.go
@@ -42,7 +42,7 @@ func RandomMoney() int64{
 
 // randomly generats a currency code
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "INR"}
+	currencies := []string{EUR, USD, INR}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
