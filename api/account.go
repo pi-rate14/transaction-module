525a8748ab7b55656e2ecbc3ff18b7cdea983219
pi-rate14 <apoorvasrivastava.14@gmail.com>
2022-09-29 21:49:13 +0530

    create and get user handlers

--- api/account.go
+++ api/account.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	db "github.com/pi-rate14/transaction-module/db/sqlc"
 )
 
@@ -28,6 +29,12 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusUnprocessableEntity, errorResponse(pqErr))
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
---
+++ db/sqlc/user_test.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/pi-rate14/transaction-module/util"
+	"github.com/stretchr/testify/require"
+)
+
+
+func createRandomUser(t *testing.T) User {
+	arg := CreateUserParams{
+		Username: util.RandomOwner(),
+		HashedPassword: "secret",
+		FullName: util.RandomOwner(),
+		Email: util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	
+	// fail if error
+	require.NoError(t, err)
+
+	// fail if empty object
+	require.NotEmpty(t, user)
+
+	// fail values are not eqal 
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.FullName, user.FullName)
+	require.Equal(t, arg.Email, user.Email)
+	
+
+	// fail if null values
+	require.True(t, user.PasswordChangedAt.IsZero())
+	require.NotZero(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	// create user
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.Username)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.FullName, user2.FullName)
+	require.Equal(t, user1.Email, user2.Email)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+}
